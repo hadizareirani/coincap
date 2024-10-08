d98f343c852a52f83d7895388c761495579e39b5
hadizareirani <22741265+hadizareirani@users.noreply.github.com>
2024-10-09 03:12:41 +0330

    added search input

--- src/app/modules/assets/assets.component.ts
+++ src/app/modules/assets/assets.component.ts
@@ -8,21 +8,29 @@ import { AssetsRepositoryService } from './services/assets-repository.service';
 import { IAssets } from './models/i-assets';
 import { map, Observable } from 'rxjs';
 import { AsyncPipe, JsonPipe } from '@angular/common';
-import { UtilsService } from '../../shared/services/utils.service';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-assets',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe],
+  imports: [
+    AsyncPipe,
+    JsonPipe,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './assets.component.html',
   styleUrl: './assets.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AssetsComponent implements OnInit {
   private assetsRepositoryService = inject(AssetsRepositoryService);
-  utilsService = inject(UtilsService);
 
   dataList$!: Observable<IAssets[]>;
+  displayedColumns: string[] = ['rank', 'name', 'symbol', 'priceUsd'];
 
   ngOnInit(): void {
     this.initialData();
@@ -32,5 +40,10 @@ export class AssetsComponent implements OnInit {
     this.dataList$ = this.assetsRepositoryService
       .getAllAssets(params)
       .pipe(map((m) => m.data));
+  }
+
+  searchHandler(event: Event) {
+    const search = (event.target as HTMLInputElement).value;
+    this.initialData({ search });
   }
 }
