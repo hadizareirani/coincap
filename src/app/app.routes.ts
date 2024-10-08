0d377d28b72b68589076e4045fd65af615d30f04
hadizareirani <22741265+hadizareirani@users.noreply.github.com>
2024-10-08 23:22:10 +0330

    init rate file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {}
+  { path: '', redirectTo: 'rates', pathMatch: 'full' },
+  {
+    path: 'rates',
+    loadComponent: () =>
+      import('./modules/rates/rates.component').then((m) => m.RatesComponent),
+  },
 ];
